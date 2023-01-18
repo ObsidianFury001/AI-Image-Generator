75f3c13c00bba81307a8054cc58a5b89b05c6a18
ObsidianFury001 <91696903+ObsidianFury001@users.noreply.github.com>
2023-01-18 22:39:36 +0400

    Added Front End UI

--- index.js
+++ index.js
@@ -7,12 +7,10 @@ const port = process.env.PORT || 5000;
 const app = express();
 // Body parser for requests 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded(  
-    { 
-        extended: false
-    }
-));
+app.use(bodyParser.urlencoded({ extended: false }));
 
+// Setting static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/openai', require('./routes/aiRoutes'));
 app.listen(port, () => { console.log(`Server started on: ${port}`)});
